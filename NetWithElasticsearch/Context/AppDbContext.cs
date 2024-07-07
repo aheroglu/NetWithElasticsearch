f10ee9722c6983bbc332b899fcb39512ac81ca28
Ahmet Hakan Eroğlu <ahmet20033134@gmail.com>
2024-07-07 15:00:58 +0300

    Add project files.

---
+++ NetWithElasticsearch/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetWithElasticsearch.Context
+{
+    public sealed class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=AHEROGLU\\SQLEXPRESS;Initial Catalog=ElasticDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        public DbSet<Travel> Travels { get; set; }
+    }
+}
+
+public sealed class Travel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
